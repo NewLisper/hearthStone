7e947b9d2218634250af3bb96f81f1319b96b1f5
fangkuaier <531326598@qq.com>
2016-03-16 19:21:11 +0800

    add cards information

---
+++ hearthStone/hearthStone/InitCardsIntoXml.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace hearthStone
+{
+    class InitCardsIntoXml
+    {
+        private static String fileLocation = "..//..//cards.xml";
+        public static IEnumerable<XElement> getAllCards()
+        {
+            XDocument doc = XDocument.Load(fileLocation);
+            IEnumerable<XElement> cardsList =
+                from c in doc.Root.Elements()
+                select c;
+            return cardsList;
+
+        }
+        public static void InitCards() {
+            XDocument card = new XDocument(
+                new XComment("cards information."),
+                new XProcessingInstruction("xml-stylesheet",
+                                 "href='mystyle.css' title='Compact' type='text/css'"),
+                new XElement("Cards",
+                    new XElement("NaiHaiChuanGong",
+                         new XElement("Crystal", "1"),
+                         new XElement("Attack", "2"),
+                         new XElement("Hp", "1")
+                     ),
+                    new XElement("JiaoXiaoZhongShi",
+                         new XElement("Crystal", "1"),
+                         new XElement("Attack", "2"),
+                         new XElement("Hp", "1")
+                     ),
+                    new XElement("NaiHaiChuanGong",
+                         new XElement("Crystal", "2"),
+                         new XElement("Attack", "2"),
+                         new XElement("Hp", "3")
+                     ),
+                    new XElement("NaiHaiChuanGong",
+                         new XElement("Crystal", "2"),
+                         new XElement("Attack", "3"),
+                         new XElement("Hp", "2")
+                     ),
+                    new XElement("NaiHaiChuanGong",
+                         new XElement("Crystal", "2"),
+                         new XElement("Attack", "2"),
+                         new XElement("Hp", "3")
+                     ),
+                    new XElement("NaiHaiChuanGong",
+                         new XElement("Crystal", "3"),
+                         new XElement("Attack", "3"),
+                         new XElement("Hp", "4")
+                     ),
+                    new XElement("NaiHaiChuanGong",
+                         new XElement("Crystal", "3"),
+                         new XElement("Attack", "3"),
+                         new XElement("Hp", "4")
+                     ),
+                    new XElement("NaiHaiChuanGong",
+                         new XElement("Crystal", "3"),
+                         new XElement("Attack", "4"),
+                         new XElement("Hp", "3")
+                     ),
+                    new XElement("NaiHaiChuanGong",
+                         new XElement("Crystal", "4"),
+                         new XElement("Attack", "4"),
+                         new XElement("Hp", "5")
+                     ),
+                    new XElement("NaiHaiChuanGong",
+                         new XElement("Crystal", "4"),
+                         new XElement("Attack", "5"),
+                         new XElement("Hp", "4")
+                     ),
+                    new XElement("NaiHaiChuanGong",
+                         new XElement("Crystal", "5"),
+                         new XElement("Attack", "5"),
+                         new XElement("Hp", "6")
+                     ),
+                    new XElement("NaiHaiChuanGong",
+                         new XElement("Crystal", "5"),
+                         new XElement("Attack", "6"),
+                         new XElement("Hp", "5")
+                     ),
+                    new XElement("NaiHaiChuanGong",
+                         new XElement("Crystal", "6"),
+                         new XElement("Attack", "6"),
+                         new XElement("Hp", "7")
+                     ),
+                    new XElement("NaiHaiChuanGong",
+                         new XElement("Crystal", "6"),
+                         new XElement("Attack", "7"),
+                         new XElement("Hp", "6")
+                     ),
+                    new XElement("NaiHaiChuanGong",
+                         new XElement("Crystal", "6"),
+                         new XElement("Attack", "6"),
+                         new XElement("Hp", "7")
+                     ),
+                    new XElement("NaiHaiChuanGong",
+                         new XElement("Crystal", "7"),
+                         new XElement("Attack", "7"),
+                         new XElement("Hp", "8")
+                     ),
+                    new XElement("NaiHaiChuanGong",
+                         new XElement("Crystal", "7"),
+                         new XElement("Attack", "7"),
+                         new XElement("Hp", "8")
+                     ),
+                    new XElement("NaiHaiChuanGong",
+                         new XElement("Crystal", "7"),
+                         new XElement("Attack", "8"),
+                         new XElement("Hp", "7")
+                     ),
+                    new XElement("NaiHaiChuanGong",
+                         new XElement("Crystal", "8"),
+                         new XElement("Attack", "8"),
+                         new XElement("Hp", "9")
+                     ),
+                    new XElement("NaiHaiChuanGong",
+                         new XElement("Crystal", "8"),
+                         new XElement("Attack", "8"),
+                         new XElement("Hp", "9")
+                     ),
+                    new XElement("NaiHaiChuanGong",
+                         new XElement("Crystal", "8"),
+                         new XElement("Attack", "9"),
+                         new XElement("Hp", "8")
+                     ),
+                    new XElement("NaiHaiChuanGong",
+                         new XElement("Crystal", "8"),
+                         new XElement("Attack", "8"),
+                         new XElement("Hp", "8")
+                     ),
+                    new XElement("NaiHaiChuanGong",
+                         new XElement("Crystal", "1"),
+                         new XElement("Attack", "2"),
+                         new XElement("Hp", "1")
+                     ),
+                    new XElement("NaiHaiChuanGong",
+                         new XElement("Crystal", "2"),
+                         new XElement("Attack", "3"),
+                         new XElement("Hp", "2")
+                     ),
+                    new XElement("NaiHaiChuanGong",
+                         new XElement("Crystal", "3"),
+                         new XElement("Attack", "3"),
+                         new XElement("Hp", "4")
+                     ),
+                    new XElement("NaiHaiChuanGong",
+                         new XElement("Crystal", "4"),
+                         new XElement("Attack", "5"),
+                         new XElement("Hp", "4")
+                     ),
+                    new XElement("NaiHaiChuanGong",
+                         new XElement("Crystal", "6"),
+                         new XElement("Attack", "5"),
+                         new XElement("Hp", "6")
+                     ),
+                    new XElement("NaiHaiChuanGong",
+                         new XElement("Crystal", "3"),
+                         new XElement("Attack", "3"),
+                         new XElement("Hp", "4")
+                     ),
+                    new XElement("NaiHaiChuanGong",
+                         new XElement("Crystal", "4"),
+                         new XElement("Attack", "5"),
+                         new XElement("Hp", "4")
+                     ),
+                    new XElement("NaiHaiChuanGong",
+                         new XElement("Crystal", "6"),
+                         new XElement("Attack", "5"),
+                         new XElement("Hp", "6")
+                     )
+                ),
+                new XComment("end of the cards.")
+            );
+            card.Declaration = new XDeclaration("1.0", "utf-8", "true");
+            Console.WriteLine(card);
+            card.Save(fileLocation);
+
+        }
+    }
+}
